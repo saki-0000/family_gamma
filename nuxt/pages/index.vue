a4b4d4bcf8a233d9a7c89066823aedd3e243c4d5
saki-0000 <saki_27_maruko@yahoo.co.jp>
2022-04-24 12:59:40 +0900

    vuetifyを利用できる

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,3 +1,11 @@
 <template>
-    <h1>Index page</h1>
+    <v-app>
+        <v-app-bar>
+            <v-app-bar-title>Family_β</v-app-bar-title>
+        </v-app-bar>
+        <v-main>
+            <div class="text-h1">Index Page</div>
+            <v-btn color="primary">ボタン</v-btn>
+        </v-main>
+    </v-app>
 </template>
