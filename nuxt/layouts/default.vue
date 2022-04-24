f554b752f3b4ce623362b62cd14541c540e1445c
saki-0000 <saki_27_maruko@yahoo.co.jp>
2022-04-24 20:37:57 +0900

    indexページのレイアウト部分をレイアウトに

---
+++ nuxt/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-app>
+        <v-app-bar>
+            <v-app-bar-title>Family_β</v-app-bar-title>
+        </v-app-bar>
+        <v-main>
+            <slot />
+        </v-main>
+    </v-app>
+</template>
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,11 +1,4 @@
 <template>
-    <v-app>
-        <v-app-bar>
-            <v-app-bar-title>Family_β</v-app-bar-title>
-        </v-app-bar>
-        <v-main>
-            <div class="text-h1">Index Page</div>
-            <v-btn color="primary">ボタン</v-btn>
-        </v-main>
-    </v-app>
+    <div class="text-h1">Index Page</div>
+    <v-btn color="primary">ボタン</v-btn>
 </template>
