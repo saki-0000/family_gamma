907175d96582f07267c3ffdc7fcf034af2842791
saki-0000 <saki_27_maruko@yahoo.co.jp>
2022-04-25 23:03:27 +0900

    簡易ログインフォーム

--- nuxt/pages/login.vue
+++ nuxt/pages/login.vue
@@ -1,6 +1,27 @@
 <script setup>
 const { title, updateTitle } = useTitle();
 updateTitle("Login Page");
+const login = () => {
+    console.log("ログインしました。");
+};
+const form = {
+    email: "",
+    password: "",
+};
 </script>
 
-<template></template>
+<template>
+    <v-form ref="form">
+        <v-text-field
+            v-model="form.password"
+            type="password"
+            label="password"
+        ></v-text-field>
+        <v-text-field
+            v-model="form.email"
+            counter="128"
+            label="email"
+        ></v-text-field
+        ><v-btn @click="login"> Login </v-btn>
+    </v-form>
+</template>
