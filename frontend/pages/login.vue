12d95ff9e0161fa043b47906dd6c03f4073be3ce
saki-0000 <saki_27_maruko@yahoo.co.jp>
2022-05-03 17:25:36 +0900

    ログイン画面

---
+++ frontend/pages/login.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-form ref="form">
+    <v-text-field
+      v-model="form.password"
+      type="password"
+      label="password"
+    ></v-text-field>
+    <v-text-field
+      v-model="form.email"
+      counter="128"
+      label="email"
+    ></v-text-field
+    ><v-btn @click="login"> Login </v-btn>
+  </v-form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  setup() {
+    const login = () => {
+      console.log("ログインしました。");
+    };
+    const form = {
+      email: "ewell.mills@example.org",
+      password: "password",
+    };
+
+    return { form, login };
+  },
+});
+</script>
