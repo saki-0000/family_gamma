5aa34e3a937d9e52650bf746e668610632a612e6
saki-0000 <saki_27_maruko@yahoo.co.jp>
2022-05-08 15:44:40 +0900

    appends profile_type_name

--- backend/app/Models/UserProfileDetail.php
+++ backend/app/Models/UserProfileDetail.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,6 +15,18 @@ class UserProfileDetail extends Model
         'profile_type_id',
         'comment',
     ];
-    protected $appends = [];
-    protected $visible = ['id', 'value', 'comment'];
+    protected $appends = ['profile_type_name'];
+    protected $visible = ['id', 'value', 'comment', 'profile_type_name'];
+
+    public function profileType()
+    {
+        return $this->belongsTo(ProfileType::class);
+    }
+
+    protected function profileTypeName(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->profileType->name,
+        );
+    }
 }
