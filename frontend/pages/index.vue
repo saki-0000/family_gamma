17f437eeb1bd3dfa756cf60b3fed74f9a5700744
saki-0000 <saki_27_maruko@yahoo.co.jp>
2022-05-09 21:16:10 +0900

    Display dynamic forms to fit the user profile

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,38 +1,35 @@
 <template>
   <div class="container">
-    <p>{{ info.text }}</p>
-    <button @click="onClicked">ボタン</button>
-    {{ data }}
-    {{ user }}
+    <div v-for="item in data" :key="item.id">
+      <v-text-field
+        v-model="item.value"
+        :rules="nameRules"
+        :counter="10"
+        :label="item.profile_type_name"
+        required
+      ></v-text-field>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import {
-  defineComponent,
-  reactive,
-  useAsync,
-  useContext,
-  useStore,
-} from "@nuxtjs/composition-api";
+import { defineComponent, useAsync, useContext } from "@nuxtjs/composition-api";
 
+export type UserProfile = {
+  id: number;
+  value: string;
+  comment: string;
+  profile_type_name: string;
+};
 export default defineComponent({
   setup() {
-    const info = reactive({
-      text: 1,
-    });
-    const onClicked = () => {
-      info.text++;
-    };
-    const { app } = useContext();
-    const data = useAsync(() => app.$axios.$get("/api").catch((err) => err));
-    const store = useStore();
+    const { $axios } = useContext();
+    const data = useAsync<UserProfile[]>(() =>
+      $axios.$get<UserProfile[]>("/api/user").catch((err) => err)
+    );
+    // const form =
 
-    // ts対応がまだ
-    /** @ts-ignore */
-    const user = store.state.auth.user;
-
-    return { info, onClicked, data, user };
+    return { data };
   },
 });
 </script>
